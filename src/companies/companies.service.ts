26d8adafc81f77bfbc843aae315d49d1f7aeaf9e
HieuLeBa <hieudeu1xx1@gmail.com>
2023-12-20 17:49:40 +0700

    update company

---
+++ src/companies/companies.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { CreateCompanyDto } from './dto/create-company.dto';
+import { UpdateCompanyDto } from './dto/update-company.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Company, CompanyDocument } from './schemas/company.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUser } from 'src/users/users.interface';
+
+@Injectable()
+export class CompaniesService {
+
+  constructor(@InjectModel(Company.name) private companyModel: SoftDeleteModel<CompanyDocument>) { }
+
+  create(createCompanyDto: CreateCompanyDto, user: IUser) {
+    return this.companyModel.create({
+      ...createCompanyDto, 
+      createdBy: {
+        _id: user._id,
+        email: user.email
+      }
+      
+    })
+  }
+
+  findAll() {
+    return `This action returns all companies`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} company`;
+  }
+
+  async update(id: string, updateCompanyDto: UpdateCompanyDto, user: IUser) {
+    return await this.companyModel.updateOne(
+      {_id: id},
+      {
+        ...updateCompanyDto,
+        updatedBy: {
+          _id: user._id,
+          email: user.email
+        }
+      }
+    )
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} company`;
+  }
+}
