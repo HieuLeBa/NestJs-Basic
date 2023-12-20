60be5900ce8bf19d44dcfb15681915801dff02a0
HieuLeBa <hieudeu1xx1@gmail.com>
2023-12-20 22:16:48 +0700

    customize message decorator

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -5,6 +5,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Company, CompanyDocument } from './schemas/company.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { IUser } from 'src/users/users.interface';
+import aqp from 'api-query-params';
 
 @Injectable()
 export class CompaniesService {
@@ -13,17 +14,41 @@ export class CompaniesService {
 
   create(createCompanyDto: CreateCompanyDto, user: IUser) {
     return this.companyModel.create({
-      ...createCompanyDto, 
+      ...createCompanyDto,
       createdBy: {
         _id: user._id,
         email: user.email
       }
-      
+
     })
   }
 
-  findAll() {
-    return `This action returns all companies`;
+  async findAll(currentPage: number, limit: number, qs: string) {
+    const { filter, sort, population } = aqp(qs);
+    delete filter.page;
+    delete filter.limit;
+
+    let offset = (+currentPage - 1) * (+limit);
+    let defaultLimit = +limit ? +limit : 10;
+    const totalItems = (await this.companyModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / defaultLimit);
+
+    const result = await this.companyModel.find(filter)
+    .skip(offset)
+    .limit(defaultLimit)
+    .sort(sort as any)
+    .populate(population)
+    .exec()
+
+    return {
+      meta: {
+      current: currentPage, //trang hiện tại
+      pageSize: limit, //số lượng bản ghi đã lấy
+      pages: totalPages, //tổng số trang với điều kiện query
+      total: totalItems // tổng số phần tử (số bản ghi)
+      },
+      result //kết quả query
+    }
   }
 
   findOne(id: number) {
@@ -43,7 +68,18 @@ export class CompaniesService {
     )
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} company`;
+  async remove(id: string, user: IUser) {
+    await this.companyModel.updateOne(
+      {_id: id},
+      {
+        deletedBy: {
+          _id:user._id,
+          email: user.email
+        }
+      }
+    )
+    return this.companyModel.softDelete({
+      _id: id
+    })
   }
 }
