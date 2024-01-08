829d60f93f6a2ffb6bd08521d57946ad8c5dd41d
HieuLeBa <hieudeu1xx1@gmail.com>
2024-01-08 15:03:48 +0700

    add cron job

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -6,6 +6,7 @@ import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 import { Subscriber, SubscriberDocument } from 'src/subscribers/schemas/subscriber.schema';
 import { Job, JobDocument } from 'src/jobs/schemas/job.schema';
 import { InjectModel } from '@nestjs/mongoose';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Controller('mail')
 export class MailController {
@@ -23,6 +24,7 @@ export class MailController {
     @Get()
     @Public()
     @ResponseMessage("Test email")
+    @Cron("0 10 0 * * 0")
     async handleTestEmail() {
         const subscribers = await this.subscriberModel.find({ });
             for (const subs of subscribers) {
